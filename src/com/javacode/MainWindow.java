b2e949adeeae1b2d0d7f3f95f57816a129c52454
iamprincejkc <princejankevin@gmail.com>
2023-08-26 15:44:11 +0800

    Swing04

--- src/com/javacode/MainWindow.java
+++ src/com/javacode/MainWindow.java
@@ -1,41 +1,32 @@
 package com.javacode;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class MainWindow {
-	private JFrame window;
+	private JFrame frame;
 
 	public MainWindow() {
-		window = new JFrame();
-		window.setTitle("Hello World");
-		window.setLayout(new BorderLayout(10, 5));
-		window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		window.setSize(800, 500);
-		window.setLocationRelativeTo(null);
+		frame = new JFrame();
+		frame.setTitle("Hello World");
+		frame.setSize(800, 500);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		JPanel panel = new JPanel();
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
-		panel.setBackground(Color.red);
+		BorderLayout bLayout = new BorderLayout();
+		bLayout.setHgap(10);
+		bLayout.setVgap(10);
+		frame.setLayout(bLayout);
 
-		Button btn1 = new Button("Button 1");
-		Button btn2 = new Button("Button 2");
-		Button btn3 = new Button("Button 3");
-		panel.add(btn1);
-		panel.add(btn2);
-		panel.add(btn3);
+		frame.add(new JButton("North"), BorderLayout.NORTH);
+		frame.add(new JButton("South"), BorderLayout.SOUTH);
+		frame.add(new JButton("West"), BorderLayout.WEST);
+		frame.add(new JButton("East"), BorderLayout.EAST);
+		frame.add(new JButton("Center"), BorderLayout.CENTER);
 
-		panel.setPreferredSize(new Dimension(250, 250));
-
-		window.add(panel, BorderLayout.WEST);
-
-		window.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
 }
